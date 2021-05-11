3a31758eb7271c2c62b7a5ca46b18b67dfb47fe7
Saurabh <saurabh.yelmame19@vit.edu>
2021-05-12 01:35:04 +0530

    append, pop, slicing, [], insert, -ve indexing checked and working properly.

--- list.cpp
+++ list.cpp
@@ -64,7 +64,7 @@ class List
 
     void append(T data)
     {
-        Node<T>* node=new Node<T>;
+        Node<T>* node=new Node<T>();
         node->setData(data);
 
         if(head==NULL)
@@ -86,65 +86,156 @@ class List
         return size;
     }
     
-    ReturnObject<T> pop(int pos=-1)
+    void insert(int pos, T value)//remember to do size++
     {
-        ReturnObject<T> obj;
-        obj.setError(0);
-        if(head==NULL)
+       // if(pos>size || pos<-size-2) // Checking if pos is valid 
+       //     cout<<"Cannot insert, as pos is excedding range of size"<<endl;
+       // else
         {
-           obj.setError(1);
-           return obj;
-        }
-        if(pos==-1)
-        {
-            Node<T>* temp1,*temp2;
-            temp1=head;
-
-            while(temp1->Next()!=NULL)
+            
+            if(pos==0 || pos<=-size)//Insert at beginning
             {
-                temp2=temp1;
-                temp1=temp1->Next();
+                Node<T>* newNode = new Node<T>();
+                newNode->setData(value);
+                newNode->setNext(head);
+                head = newNode;
             }
-            temp2->setNext(NULL);
-            obj.setData(temp1->Data());
-            delete temp1;
-            size--;
+            else if(pos>=size)   //Insert at end
+                append(value);
 
-            return obj;
-        }
-        if(pos==0)
-        {
-            obj.setData(head->Data());
-            Node<T>* temp;
-            temp=head;
-            head=head->Next();
-            delete temp;
-            size--;
-            return obj;
+            else    // Insert at any middle position
+            {
+                pos = convertNegativeIndexToPositive(pos);
+                int count=0;
+                Node<T>* temp = head;
+
+                while(count<pos-1)
+                {
+                    temp = temp->Next();
+                    count++;
+                }
+                Node<T>* newNode = new Node<T>();
+                newNode->setData(value);
+                newNode->setNext(temp->Next());
+                temp->setNext(newNode);
+                    
+            }
+                
+            size++;
         }
         
-        if(pos>=size)
-        {
-            obj.setError(1);
-            return obj;
-        }
+
+    }
+
+    int index(T val, int start=0, int end=-1)
+    {
+        //Will return -1 if element is not found
+        //Will return -2 if Start index is greater than size
+        //Will return -3 if End index is smaller than start
+        //Will return -1 if element is not found
+        start = convertNegativeIndexToPositive(start);
+        if(end==-1)
+            end = size;
+        end = convertNegativeIndexToPositive(end);
+
+        if(head==NULL)
+            {cout<<"List is Empty"<<endl;return -1;}
+        if(start>=size)
+            {cout<< "Start index is greater than size!"<<endl;return -2;}
+        else if( end<start)
+            {cout<< "End index is smaller than start!"<<endl;return -3;}
 
         else
         {
+            cout<<"\nStarted search from "<< start << " to "<<end<<endl;
             int count=0;
-            Node<T>* temp=head, *temp1;
-            while(count<pos)
+            Node<T>* temp = head;
+
+            while(count<start)
             {
-                temp1=temp;
-                temp=temp->Next();
+                temp = temp->Next();
                 count++;
             }
-            obj.setData(temp->Data());
-            temp1->setNext(temp->Next());
-            delete temp;
-            size--;
-            return obj;
+            while(count<end)
+            {
+                if(temp->Data()==val)
+                    return count;
+                if(temp->Next()==NULL)
+                    return -1;
+                temp = temp->Next();
+                count++;
+            }
+            return -1;// Element not found
         }
+    }
+
+
+    ReturnObject<T> pop(int pos=-1)//remember to do size--
+    {
+        // setError(0) : if there is no error
+        // setError(1) : if list is empty
+        // setError(2) : if pos is not in the range of size
+
+        ReturnObject<T> obj;
+        obj.setError(0);
+
+        if(pos>=size || pos<-size)
+            obj.setError(2);
+            
+        else
+        {
+            pos = convertNegativeIndexToPositive(pos);
+        
+            if(head==NULL)
+                obj.setError(1);
+            
+            else if(pos==-1 || pos==size-1) //As we have converted the negative index to psitive, just check pos==size-1
+            {
+                Node<T>* temp1,*temp2;
+                temp1=head;
+
+                while(temp1->Next()!=NULL)
+                {
+                    temp2=temp1;
+                    temp1=temp1->Next();
+                }
+                temp2->setNext(NULL);
+                obj.setData(temp1->Data());
+                delete temp1;
+                size--;
+            }
+            
+            else if(pos==0)
+            {
+                obj.setData(head->Data());
+                Node<T>* temp;
+                temp=head;
+                head=head->Next();
+                delete temp;
+                size--;
+            }
+        
+        
+            else
+            {
+                int count=0;
+                Node<T> *temp=head, *temp1=head;
+                while(count<pos)
+                {
+                    temp1=temp;
+                    temp=temp->Next();
+                    count++;
+                }
+                obj.setData(temp->Data());
+                temp1->setNext(temp->Next());
+                delete temp;
+                size--;
+            }
+        
+        
+        }
+    
+        return obj;
     }
 
     void display()
@@ -154,7 +245,7 @@ class List
         cout<<">> ";
         if(temp==NULL)
         {
-            cout<<"EMPTY"<<endl;
+            cout<<"List is EMPTY"<<endl;
             return;
         }
         while(temp!=NULL)
@@ -164,4 +255,78 @@ class List
         }
         cout<<endl;
     }
+    
+    int convertNegativeIndexToPositive(int index)
+    {   //If index is negative, it will return corresponding positive index
+        //else will return the positive index itself
+        if(index<0)
+            return size+index;
+        return index;
+
+    }
+    
+    ReturnObject<T> operator[](int index)
+    {
+        //error is 1 if index exceeds size
+        //error is 2 if list is empty
+        //error is 0 if there is no error
+        index = convertNegativeIndexToPositive(index);
+        ReturnObject<T> obj;
+        if(index>size)
+        {
+            obj.setError(1);
+            return obj;
+        }
+        if(head==NULL)
+        {
+            obj.setError(2);
+            return obj;
+        }
+
+        Node<T>* temp = head;
+        int count=0;
+        while(count!=index)
+        {
+            temp = temp->Next();
+            count++;
+        }
+        //cout<<"Count: "<<count<<"  Index: "<<index<<endl;
+        obj.setError(0);
+        obj.setData(temp->Data());
+        return obj;
+    }
+    
+    List<T> operator()(int start,int end,int step=1)
+    {
+        int counter = 0;
+        List<T> newlist;
+
+        Node<T>* temp = head;
+        while(counter<start)
+        {
+            temp = temp->Next();
+            counter++;
+        }
+
+        while(counter<end)
+        {
+            newlist.append(temp->Data());
+            //cout<<"Appended: "<<temp->Data()<<endl;
+
+            for(int j=0;j<step;j++){
+                if(temp==NULL)
+                    break;
+                temp = temp->Next();
+            }
+                
+
+            counter+=step;
+        }
+        
+        return newlist;
+
+    }
+
+
+    
 };
--- test.cpp
+++ test.cpp
@@ -1,29 +1,69 @@
 #include<iostream>
 #include "list.cpp"
+#include<vector>
 using namespace std;
 
 int main()
 {
-    List<int> l;
-    l.display();
-    l.append(1);
-    l.append(2);
-    l.append(3);
-    l.append(4);
-    l.append(5);
-    l.append(6);
-    l.append(7);
-    l.append(8);
-    l.display();
-    l.pop();
-    l.display();
-    l.pop(1);
-    l.pop(4);
-    l.display();
-    cout<<l.pop(10).Error()<<endl;
-    cout<<l.len()<<endl;;
-    cout<<l.pop(1).Data()<<endl;
+    system("cls");
+
+    List<char> l;
+    for(int i=0;i<9;i++)
+        l.append(i+'A');
+
+    //Negative Indexes
+    cout<<">> ";
+    for(int i=l.len();i>0;i--)
+        cout<<i<<" ";
+    cout<<endl;
+
+    //Positive Indexes
+    cout<<">> ";
+    for(int i=0;i<l.len();i++)
+        cout<<i<<" ";
+    cout<<endl;
+
     l.display();
 
+    /*
+   ReturnObject<char> poppedObj = l.pop(-99);
+    if(poppedObj.Error()==0)
+        cout<<"Popped Value : "<<poppedObj.Data()<<endl; 
+    else if(poppedObj.Error()==1)
+        cout<<"Cannot pop as List is EMPTY"<<endl;
+    else if(poppedObj.Error()==2)
+        cout<<"Cannot pop as passed position is out of SIZE"<<endl;
+    l.display();
+    */
+
+   
+    /*l.insert(-99,'*');  
+    l.display();*/
+    int answer = l.index('D',0,99);
+    cout<<"Index of D is : "<<answer<<endl;
+   /*l.index('Z');//0 to 9
+   l.index('Z',2,4);//0 to 4
+   l.index('Z',-4);//5 to 9
+   l.index('Z',-6,-3);//3 to 6
+   l.index('Z',2,55);// 2 to 55*/
+    /*List<char> subList;
+    subList = l(2,9,3);
+    subList.display();
+    */
+
+   /*
+   int answer = l[-5].Error();
+   if(answer==0)
+    cout<<"Element at index is : " << l[-5].Data()<<endl;
+   if(answer==1)
+    cout<<"Index is exceeding size"<<endl;
+   if(answer==2)
+    cout<<"List is EMPTY!"<<endl;
+    */
+   
+    cout<<endl<<endl;
     return 0;
+
+
+
 }
